9f796ee1428508adb70c31366e06b3d0805f1979
Jonny <zhenghongju@zhenghongjudeMacBook-Pro.local>
2020-04-24 15:43:04 +0800

    addTwoNumber

--- 2_addTwoNumbers.js
+++ 2_addTwoNumbers.js
@@ -12,6 +12,42 @@
 * @return {ListNode}
 */
 
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
 var addTwoNumbers = function (l1, l2) {
-  
+  var c1 = l1;
+  var c2 = l2;
+  var l3;
+  var c3;
+  var carry = 0;
+
+  while (c1 || c2 || carry) {
+    var v1 = 0;
+    var v2 = 0;
+
+    if (c1) {
+      v1 = c1.val;
+      c1 = c1.next;
+    }
+
+    if (c2) {
+      v2 = c2.val;
+      c2 = c2.next;
+    }
+
+    var sum = v1 + v2 + carry;
+    carry = Math.floor(sum / 10);
+
+    if (!c3) {
+      l3 = new ListNode(sum % 10);
+      c3 = l3;
+    } else {
+      c3.next = new ListNode(sum % 10);
+      c3 = c3.next;
+    }
+  }
+  console.log('result:',l3);
 };
